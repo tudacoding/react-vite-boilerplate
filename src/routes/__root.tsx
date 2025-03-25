92693cf9671caf415218c57c1b3679b04cda7ffe
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2025-03-25 14:55:04 +0700

    add tanstack query devtool

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
       <TanStackRouterDevtools />
+      <ReactQueryDevtools initialIsOpen={false} />
     </>
   ),
 });
